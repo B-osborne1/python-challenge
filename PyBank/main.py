c79277e348fa56f35183dde38997f62e6d60c3bd
Brett Osborne <bosb019@gmail.com>
2024-01-02 14:00:35 +1100

    PyBank update. Main code without export

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,43 +1,40 @@
-import os
 import csv
 
-    #Path to main file
-pybank_csv = os.path.join("Resources", "budget_data.csv")
+# open the file in read mode
+filename = open('Resources/budget_data.csv', 'r')
+ 
+# creating dictreader object
+file = csv.DictReader(filename)
+ 
+# creating empty lists
+month = []
+totalprofit = []
 
+# allowing for total variable to change
+total = 0
 
-row_count = 0
-column_count = 1
-column_1 = 1
-column_sum = 0
+# values to empty list through column iterations
+for col in file:
+    month.append(col['Date'])
+    totalprofit.append(int(col['Profit/Losses']))
 
-    #Title
-print("Financial Analysis")
-print("--------------------------------")
+# Calculate total of profit column
+for element in totalprofit:
+        total += int(element)
     
-    #Open main file
-with open(pybank_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
 
-    #Ignore the initial title row
-    next(csvreader, None)
+average_change = round(total/len(month),2)
+min_row_month = month[totalprofit.index(min(totalprofit))]
+max_row_month = month[totalprofit.index(max(totalprofit))]
+min_row_value = totalprofit[totalprofit.index(min(totalprofit))]
+max_row_value = totalprofit[totalprofit.index(max(totalprofit))]
 
-    #Part 1: Total number of months - Sum of rows used excluding title
-    for row in csvreader:
-        if len(row) > column_count:
-            row_count += 1
-
-
-    print(f'Total number of months = {row_count}')
-#Look back at this -- current failure
-                    #Part 2: Net gain/loss - Sum of values in rows excluding title
-                    for row in csvreader:
-                        if len(row) > column_sum and row[column_1]:
-                            column_sum += float(row[column_1])
-
-                    print(f'Total profit over period = {column_sum}')
-    #Part 3: Average change per month
-
-
-
-    #Part 4: Greatest increase/decrease (name and quantity)
+# printing lists
+print("Financial Analysis") #Title
+print("---------------------") #Spacer
+print("Total months: ", len(month)) #Number of months in sheet
+print("Overall profit/loss: ", total) #Overall change in value from start
+print("Average monthly change", average_change) #Average change of value per month
+print(f'Month with minimum profit/loss: {min_row_month} ( {min_row_value} )') #minimum value and corresponding month
+print(f'Month with maximum profit/loss: {max_row_month} ( {max_row_value} )') #maximum value and corresponding month
 
